918a335fa7b1ae56732a54201a0360acc914180c
Philip Claesson <philip.claesson@northvolt.com>
2023-12-24 01:33:58 +0100

    collision handler

--- src/physics.ts
+++ src/physics.ts
@@ -5,7 +5,8 @@ const Engine = Matter.Engine,
     Render = Matter.Render,
     Runner = Matter.Runner,
     Bodies = Matter.Bodies,
-    Composite = Matter.Composite;
+    Composite = Matter.Composite,
+    Events = Matter.Events;
 
 let engine: Matter.Engine;
 let render: Matter.Render;
@@ -14,11 +15,6 @@ export function loadPhysics() {
 
     // create an engine
     engine = Engine.create();
-
-    // get window size
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    console.log(`width: ${width}, height: ${height}`)
 
     // Create a renderer
     render = Render.create({
@@ -34,6 +30,7 @@ export function loadPhysics() {
     // create two boxes and a ground
     const ball = Bodies.circle(400, 200, 80, {
         restitution: 0.6, // Bounciness
+        label: 'Ball',
         render: {
             fillStyle: 'red'
         }
@@ -42,10 +39,13 @@ export function loadPhysics() {
 
     // Add walls
     const wallThickness = 10;
-    createWall(window.innerWidth / 2, wallThickness / 2, window.innerWidth, wallThickness, 'red'); // Top
-    createWall(window.innerWidth / 2, window.innerHeight - wallThickness / 2, window.innerWidth, wallThickness, 'blue'); // Bottom
-    createWall(wallThickness / 2, window.innerHeight / 2, wallThickness, window.innerHeight, 'green'); // Left
-    createWall(window.innerWidth - wallThickness / 2, window.innerHeight / 2, wallThickness, window.innerHeight, 'yellow'); // Right
+    createWall(window.innerWidth / 2, wallThickness / 2, window.innerWidth, wallThickness, 'red', 'Top'); // Top
+    createWall(window.innerWidth / 2, window.innerHeight - wallThickness / 2, window.innerWidth, wallThickness, 'blue', 'Bottom'); // Bottom
+    createWall(wallThickness / 2, window.innerHeight / 2, wallThickness, window.innerHeight, 'green', 'Left'); // Left
+    createWall(window.innerWidth - wallThickness / 2, window.innerHeight / 2, wallThickness, window.innerHeight, 'yellow', 'Right'); // Right
+
+    // Collision handling
+    Events.on(engine, 'collisionStart', handleCollision);
 
     // run the renderer
     Render.run(render);
@@ -55,13 +55,13 @@ export function loadPhysics() {
 
     // run the engine
     Runner.run(runner, engine);
-    // fit the render viewport to the scene
     console.log('Physics loaded')
 }
 
-function createWall(x: number, y: number, width: number, height: number, color: string) {
+function createWall(x: number, y: number, width: number, height: number, color: string, label: string) {
     const wall = Bodies.rectangle(x, y, width, height, {
         isStatic: true,
+        label: label,
         render: { fillStyle: color }
     });
     Composite.add(engine.world, [wall]);
@@ -72,6 +72,34 @@ export function setGravity(x: number, y: number) {
         engine.gravity.x = x;
         engine.gravity.y = y;
     }
+}
+
+function handleCollision(event: Matter.IEventCollision<Matter.Engine>) {
+    const source = event.source
+    let bodyLabel: string = ''
+    if (source == null)
+        return;
+    const pairs: any = source.pairs
+    if (pairs == null)
+        return;
+    const collisions: any = pairs.list
+    if (collisions == null)
+        return;
+    if (!Array.isArray(collisions)) {
+        console.log('collisions: Not an array')
+        return;
+    }
+    if (!collisions.length) {
+        console.log('collisions: Empty array')
+        return;
+    }
+    if (!collisions[0].bodyB) {
+        console.log('collisions: BodyB is null', collisions[0])
+        return;
+    }
+    bodyLabel = collisions[0].bodyB.label
+
+    console.log(`ball collided with ${bodyLabel}`)    
 }
 
 // Resize handling
